fa30746f22059a1c964252d6d2a829ce98d01b87
nphatcampcode <72789405+nphatcampcode@users.noreply.github.com>
2020-11-25 00:21:34 +0700

    Add files via upload

---
+++ tong100chuso/tong100.java
@@ -0,0 +1,13 @@
+package tong100chuso;
+
+public class tong100 {
+	public static void main(String[] arg)	{
+		int a;
+		int i=0;
+		for (a=1;a<=100;a++)	{
+			i = i + a;
+			System.out.print("\n " +i);
+		}
+	}
+
+}
